874df2dca280b242ab068de0310aab79275abcd1
dernasherbrezon <dernasherbrezon@72ae4f1e-f84d-24be-197a-7ce176699613>
2012-12-21 07:54:35 +0000

    logging added

--- maven/install.sh
+++ maven/install.sh
@@ -3,6 +3,6 @@
 DIRNAME=`dirname $0`
 cd $DIRNAME
 
-/usr/share/maven3/bin/mvn install:install-file -Dfile=../simple-csrf-filter/target/simple-csrf-filter-2.4.jar -DgroupId=com.google.code -DartifactId=simple-csrf-filter -Dversion=2.4 -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
-/usr/share/maven3/bin/mvn install:install-file -Dfile=../simple-csrf-filter/target/simple-csrf-filter-2.4-sources.jar -DgroupId=com.google.code -DartifactId=simple-csrf-filter -Dversion=2.4 -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=. -Dclassifier=sources
+/usr/share/maven3/bin/mvn install:install-file -Dfile=../simple-csrf-filter/target/simple-csrf-filter-2.5.jar -DgroupId=com.google.code -DartifactId=simple-csrf-filter -Dversion=2.5 -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
+/usr/share/maven3/bin/mvn install:install-file -Dfile=../simple-csrf-filter/target/simple-csrf-filter-2.5-sources.jar -DgroupId=com.google.code -DartifactId=simple-csrf-filter -Dversion=2.5 -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=. -Dclassifier=sources
                           
