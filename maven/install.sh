d361c3946bedb9c0295e1fa9ef1bcb3e8465d942
dernasherbrezon <dernasherbrezon@72ae4f1e-f84d-24be-197a-7ce176699613>
2013-09-09 05:58:35 +0000

    install to maven repo + eclipse run configuration instead of install.sh

--- maven/install.sh
+++ maven/install.sh
@@ -3,6 +3,6 @@
 DIRNAME=`dirname $0`
 cd $DIRNAME
 
-/usr/share/maven3/bin/mvn install:install-file -Dfile=../simple-csrf-filter/target/simple-csrf-filter-2.6.jar -DgroupId=com.google.code -DartifactId=simple-csrf-filter -Dversion=2.6 -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
-/usr/share/maven3/bin/mvn install:install-file -Dfile=../simple-csrf-filter/target/simple-csrf-filter-2.6-sources.jar -DgroupId=com.google.code -DartifactId=simple-csrf-filter -Dversion=2.6 -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=. -Dclassifier=sources
+/usr/share/maven3/bin/mvn install:install-file -Dfile=../simple-csrf-filter/target/simple-csrf-filter-2.7.jar -DgroupId=com.google.code -DartifactId=simple-csrf-filter -Dversion=2.7 -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=.
+/usr/share/maven3/bin/mvn install:install-file -Dfile=../simple-csrf-filter/target/simple-csrf-filter-2.7-sources.jar -DgroupId=com.google.code -DartifactId=simple-csrf-filter -Dversion=2.7 -Dpackaging=jar -DcreateChecksum=true -DlocalRepositoryPath=. -Dclassifier=sources
                           
